4992fdd1112a86fa3ea992c975224778e43dda0d
ManikBansal07 <92663395+ManikBansal07@users.noreply.github.com>
2022-01-24 11:49:13 +0530

    Add files via upload

---
+++ Asg2_CSE_21103108_Output.py
@@ -0,0 +1,45 @@
+#Question 1
+
+Length of the string is 35
+Reversed string is egaugnal evitisnes esac a si nohtyP
+Index of 'a' is  10
+The string after removing whitespaces is Pythonisacasesensitivelanguage
+
+#Question 2
+
+Hey, Manik Bansal Here!
+My SID is 21103108
+I am from Computer Science and Engineering department and my CGPA is 9.9
+
+#Question 3
+
+8
+58 
+50 
+224
+40 
+14 
+0
+
+#Question 4
+
+Please enter three numbers
+a = 2
+b = 7
+c = 4
+The greatest number is b = 7
+
+#Question 5
+
+Please enter the string here 
+My name is Manik
+Yes
+
+#Question 6
+
+Please enter three sides of a triangle
+Side 1 = 3
+Side 2 = 4
+Side 3 = 5
+Yes
+
